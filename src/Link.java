3f78f9b1018831e4678dd86daec8d5765b5d8452
BriMarie <briannarodriguez0529@gmail.com>
2020-04-07 14:47:43 -0500

    Change the design for links and nodes

--- src/Link.java
+++ src/Link.java
@@ -1,20 +1,31 @@
 import java.util.Collections;
-import java.util.HashSet;
+//import java.util.HashSet;
 
 class Link {
 
   Node nextNode;
+  String validInput;
+  //only one validInput per link rather than LinkedList
+  //a string is used for certain cases, like when the input is non-newline
 
-  //HashSet<String> validInputs;
-  char validInputs;
-  boolean checkInput(String fileInput) {
-    //TODO
-    return true; //temporary line only; replace later
+  Link(String validInput, Node nextNode) {
+    this.validInput = validInput;
+    this.nextNode = nextNode;
   }
 
-  Link(/*HashSet<String>*/Node nextNode, char validInputs) {
-    this.validInputs = validInputs;
-    this.nextNode = nextNode;
+  boolean checkInput(String fileInput) {
+    switch(fileInput) {
+      case "non-*":
+        if(fileInput[0] != '*')
+          return true;
+      case "non-newline":
+        if(fileInput[0] != '\n')
+          return true;
+      default:
+        if(fileInput[0] == this.validInput)
+          return true;
+    }
+    return false;
   }
 
 }
--- src/Node.java
+++ src/Node.java
@@ -6,18 +6,20 @@ class Node {
 
   public boolean isFinal;
   public int numLink;
-  //HashSet<Link> links;
-  Link links[];
-  Node(boolean isFinal, Link links[]) {
+  HashSet<Link> links;
+  //I'm using a hashset because an array requires you know how many elements it will contain
+  //Link links[];
+
+  Node(boolean isFinal) {
     this.isFinal = isFinal;
     //this.numLink = numLink;
-    this.links = links;
+    //this.links = links;
+  }
 
-
+  public void addLink(String input, Node node) {
+    links.add(new Link(input, node));
   }
   //public void add(char x, Node y) {
    // Link lnk = new Link(x,y);
   //}
 }
-
-
