7b3102d124e10b1ea4115787425a494ecaee8416
BriMarie <briannarodriguez0529@gmail.com>
2020-05-07 16:21:59 -0500

    Create indenting system

--- src/parser.java
+++ src/parser.java
@@ -1,14 +1,16 @@
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 
-public class parser {
+public class Parser {
+	
     public static void main(String args[]) {
-        //TODO
+		
         Scanner sc = new Scanner();
 
         Scanner.Graph gra = new Scanner.Graph();
-        parser ps = new parser();
+        Parser ps = new Parser();
 
         String output = "";
         try (FileReader fr = new FileReader("test text.txt")) {
@@ -37,168 +39,220 @@ public class parser {
 
 
     }
+	
     String[] strArray(String str){
         String[] ary = str.split(" ");
         return ary;
     }
 
     void program(String[] ary) {
-        System.out.println("<Program>");
-        stmt_list(ary);
-        System.out.println("</Program>");
+        System.out.printf("<Program>\n");
+        stmt_list(ary, 1);
+        System.out.printf("</Program>\n");
     }
-    void stmt_list(String[] ary) {
-        if (ary != null) {
-            System.out.println("    <stmt_list>");
-            stmt(ary);
-            System.out.println("    </stmt_list>");
-        }else{return;};
-
+	
+    void stmt_list(String[] ary, int n) {
+        if (ary.length > 0) {
+			indent(n);
+            System.out.printf("<stmt_list>\n");
+            stmt(ary, n+1);
+			indent(n);
+            System.out.printf("</stmt_list>\n");
+			stmt_list(Arrays.copyOfRange(ary, 1, ary.length), n+1);
+        }else{
+			return;
+		}
     }
 
-    void stmt(String[] ary){
-        System.out.println("        <stmt>");
-        if(ary[0].equals("read")){
-            readFnk(ary);
-            if(ary[1].equals("id")){
-
-                identifier(ary,3);
+    void stmt(String[] ary, int n){
+		indent(n);
+        System.out.printf("<stmt>\n");
+		//indent(n+1);
+		//System.out.printf(ary[0] + "\n");
+        if(ary[0].contains("read")){
+            readFnk(ary, n+1);
+            if(ary[1].contains("id")){
+                identifier(ary, n+1, 3);
             }
         }
-        else if(ary[0].equals("write")){
-            writeFnk(ary);
+        else if(ary[0].contains("write")){
+            writeFnk(ary, n+1);
         }
-        else if(ary[1].equals("id")){
+        else if(ary[1].contains("id")){
 
-            if(ary[3].equals("assign")){
-                System.out.println("/////////////////////");
-                expr(ary,5);
+            if(ary[3].contains("assign")){
+                System.out.printf("/////////////////////\n");
+                expr(ary, n+1, 5);
             }
         }
-        System.out.println("        </stmt>");
+		indent(n);
+        System.out.printf("</stmt>\n");
     }
-    void readFnk(String[] ary){
-        System.out.println("            <read>");
-        System.out.println("                " + ary[0]);
-        System.out.println("            </read>");
+	
+    void readFnk(String[] ary, int n){
+		indent(n);
+        System.out.printf("<read>\n");
+		indent(n+1);
+        System.out.printf(ary[0] + "\n");
+		indent(n);
+        System.out.printf("</read>\n");
     }
-    void identifier(String[] ary,int i){
-        System.out.println("            <id>");
-        System.out.println("                "+ary[i-1]);
-        System.out.println("            </id>");
-    }
-
-    void numId(String[] ary,int i){
-        System.out.println("            <number>");
-        System.out.println("                "+ary[i-1]);
-        System.out.println("            </number>");
-    }
-
-    void writeFnk(String[] ary){
-
-        System.out.println("            <write>");
-        System.out.println("                " + ary[0]);
-        System.out.println("            </write>");
-        expr(ary,3);
+	
+    void identifier(String[] ary, int n, int i){
+		indent(n);
+        System.out.printf("<id>\n");
+		indent(n+1);
+        System.out.printf(ary[i-1] + "\n");
+		indent(n);
+        System.out.printf("</id>\n");
     }
 
-    int expr(String[] ary, int i){
-        System.out.println("                <expr>");
-        i =term(ary,i);
-        i = term_tail(ary,i+2);
-        System.out.println("                </expr>");
+    void numId(String[] ary, int n, int i){
+		indent(n);
+        System.out.printf("<number>\n");
+		indent(n+1);
+        System.out.printf(ary[i-1] + "\n");
+		indent(n);
+        System.out.printf("</number>\n");
+    }
+
+    void writeFnk(String[] ary, int n){
+		indent(n);
+        System.out.printf("<write>\n");
+		indent(n+1);
+        System.out.printf(ary[0] + "\n");
+		indent(n);
+        System.out.printf("</write>\n");
+        expr(ary, n, 3);
+    }
+
+    int expr(String[] ary, int n, int i){
+		indent(n);
+        System.out.printf("<expr>\n");
+        i = term(ary, n+1, i);
+        i = term_tail(ary, n+1, i+2);
+        System.out.printf("</expr>\n");
         return i;
     }
 
-    int term(String[] ary, int i){
-        System.out.println("                    <term>");
-        i =factor(ary,i);
-        fact_tail(ary,i+2);
-        System.out.println("                    </term>");
+    int term(String[] ary, int n, int i){
+		indent(n);
+        System.out.printf("<term>\n");
+        i = factor(ary, n+1, i);
+        fact_tail(ary, n+1, i+2);
+        System.out.printf("</term>\n");
         return i;
     }
 
-    int term_tail(String[] ary,int i){
-        System.out.println("                    <term_tail>");
+    int term_tail(String[] ary, int n, int i){
+		indent(n);
+        System.out.printf("<term_tail>\n");
         if(i+2<= ary.length) {
-            if ((ary[i].equals("plus")) || (ary[i].equals("minus"))) {
+            if ((ary[i].contains("plus")) || (ary[i].contains("minus"))) {
 
-                add_op(ary, i);
-                i = term(ary, i + 2);
-                term_tail(ary, i);
+                add_op(ary, n+1, i);
+                i = term(ary, n+1, i + 2);
+                term_tail(ary, n+1, i);
 
             }
         }
-        System.out.println("                    </term_tail>");
+		indent(n);
+        System.out.printf("</term_tail>\n");
             return i;
     }
 
-    int factor(String[] ary, int i){
-        System.out.println("                    <factor>");
+    int factor(String[] ary, int n, int i){
+		indent(n);
+        System.out.printf("<factor>\n");
         //lparen <expr> rparen |id|number
-        if(ary[i].equals("lParen")){
-            System.out.println(ary[i-1]);
-            i = expr(ary,i+2);
+        if(ary[i].contains("lParen")){
+			indent(n+1);
+            System.out.printf(ary[i-1]);
+            i = expr(ary, n+1, i+2);
             //System.out.println(")");
-            System.out.println(ary[i+3]);
-            System.out.println("                    </factor>");
+			indent(n+1);
+            System.out.printf(ary[i+3]);
+            System.out.printf("</factor>\n");
 
             return i+4;
         }
-        else if(ary[i].equals("id")){
-            identifier(ary,i);
+        else if(ary[i].contains("id")){
+            identifier(ary, n+1, i);
         }
-        else if(ary[i].equals("number")){
-            numId(ary,i);
+        else if(ary[i].contains("number")){
+            numId(ary, n+1, i);
         }
-        System.out.println("                    </factor>");
+		indent(n);
+        System.out.printf("</factor>\n");
         return i;
     }
 
-    int fact_tail(String[] ary, int i){
-        System.out.println("                    <fact_tail>");
+    int fact_tail(String[] ary, int n, int i){
+		indent(n);
+        System.out.printf("<fact_tail>\n");
         //<mult_op> <factor> <fact_tail> | e
         if((i+2)<= ary.length) {
-        if((ary[i].equals("times"))||(ary[i].equals("div"))) {
-
-                mult_op(ary, i);
-                i = factor(ary, i + 2);
-                fact_tail(ary, i);
+        if((ary[i].contains("times"))||(ary[i].contains("div"))) {
+                mult_op(ary, n+1, i);
+                i = factor(ary, n+1, i + 2);
+                fact_tail(ary, n+1, i);
             }
         }
-
-        System.out.println("                    </fact_tail>");
+		indent(n);
+        System.out.printf("</fact_tail>\n");
         return i;
     }
 
-    void add_op(String[] ary, int i){
-        System.out.println("                        <add_op>");
-        if(ary[i].equals("plus")){
-            System.out.println("                            <plus>");
-            System.out.println("                                "+ary[i-1]);
-            System.out.println("                            </plus>");
+    void add_op(String[] ary, int n, int i){
+		indent(n);
+        System.out.printf("<add_op>\n");
+        if(ary[i].contains("plus")){
+			indent(n+1);
+            System.out.printf("<plus>");
+			indent(n+2);
+            System.out.printf(ary[i-1] + "\n");
+			indent(n+1);
+            System.out.printf("</plus>" + "\n");
         }
-        else if(ary[i].equals("minus")){
-            System.out.println("                            <minus>");
-            System.out.println("                                "+ary[i-1]);
-            System.out.println("                            </minus>");
+        else if(ary[i].contains("minus")){
+			indent(n+1);
+            System.out.printf("<minus>\n");
+			indent(n+2);
+            System.out.printf(ary[i-1] + "\n");
+			indent(n+1);
+            System.out.printf("</minus>");
         }
-
-        System.out.println("                        </add_op>");
+		indent(n);
+        System.out.printf("</add_op>\n");
     }
 
-    void mult_op(String[] ary, int i){
-        System.out.println("                        <mult_op>");
-        if(ary[i].equals("times")) {
-            System.out.println("                            <times>");
-            System.out.println("                                " + ary[i - 1]);
-            System.out.println("                            </times>");
-            System.out.println("                        </mult_op>");
+    void mult_op(String[] ary, int n, int i){
+		indent(n);
+        System.out.printf("<mult_op>\n");
+        if(ary[i].contains("times")) {
+			indent(n+1);
+            System.out.printf("<times>\n");
+			indent(n+2);
+            System.out.printf(ary[i - 1] + "\n");
+			indent(n+1);
+            System.out.printf("</times>\n");
+			indent(n);
+            System.out.printf("</mult_op>\n");
         }
-        else if(ary[i].equals("div")){
-            System.out.println("                            <div>");
-            System.out.println("                                "+ary[i-1]);
-            System.out.println("                            </div>");
+        else if(ary[i].contains("div")){
+			indent(n+1);
+            System.out.printf("<div>\n");
+			indent(n+2);
+            System.out.printf(ary[i-1] + "\n");
+			indent(n+1);
+            System.out.printf("</div>\n");
         }
     }
+	
+	void indent(int n) {
+		for(int i = 0; i < n; i++) {
+			System.out.printf("\t");
+		}
+	}
+	
 }
